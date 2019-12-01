1ae0461fa303bccf50af751d38d836a424b03bb2
mjconns <mjconns@gmail.com>
2019-12-01 17:37:21 -0600

    adding new assignment

---
+++ week-1/connelly-assignment-1.4.js
@@ -0,0 +1,34 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Micah Connelly
+; Date: 01 December 2019
+; Description: Primitive Types
+;===========================================
+*/
+
+//start program
+
+/*
+    Expected output:
+    This will display all the declared variables
+    in a cohesive sentence from the console log
+*/
+
+//variable declaration and assignment goes here...
+
+var firstName = 'Micah';
+var lastName = 'Connelly';
+var age = 31;
+var street = 'Random street 123';
+var city = 'Zomaha';
+var zipCode = 45678;
+
+console.log(
+
+    firstName +' '+ lastName + ' lives at ' + street +' in '+ city +', '+ zipCode 
+    
+    );
+
+
+//end program
