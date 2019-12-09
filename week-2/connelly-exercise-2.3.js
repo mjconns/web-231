1da760d75ed57da7f7fe0629ace94e1122b50b14
mjconns <mjconns@gmail.com>
2019-12-08 18:06:28 -0600

    adding exercise 2.3

---
+++ week-2/connelly-exercise-2.3.js
@@ -0,0 +1,33 @@
+/*
+============================================
+; Title:  week 2: exercise 2.3
+; Author: Micah Connelly
+; Date:   8 December 2019
+; Modified By: Micah Connelly
+; Description: displaying my name
+;===========================================
+*/
+
+/*
+  Expected output: FirstName, LastName, title, today's date
+*/
+
+// import
+const header = require('./header.js');
+
+// function properties
+const greeting = "Hello"
+const punctuation = "!"
+myName.firstName = "Micah";
+myName.lastName = "Connelly"
+
+// function
+function myName() {
+  return myName;
+}
+
+// output
+console.log('\n');
+console.log(greeting + " " + myName.firstName + " " + myName.lastName + punctuation);
+
+// end program
