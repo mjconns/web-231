5f98af60f452432746efbaa0783503e1a85e8484
mjconns <mjconns@gmail.com>
2019-12-15 15:11:41 -0600

    pierce reply added

---
+++ week-3/discussion-3.1-Pierce-reply.js
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title: Discussion 3.1
+; Author: Kimberly Pierce
+; Date:  11 December 2019
+; Modified By: Micah Connelly
+; Description: Control statement with two errors
+;===========================================
+*/
+
+
+var i = 1; //declare variable value to be used in loop
+
+while (i < 11) { //condition for loop to continue
+  console.log(i + " " + "This loop will count to 10"); // the thing to do
+    i++; //increase var value by 1 each time
+  }
+
+
