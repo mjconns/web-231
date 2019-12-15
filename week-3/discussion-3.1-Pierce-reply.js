4c7a6fb1e4ce2a2d6260fb44f006f45a6ce1b36d
mjconns <mjconns@gmail.com>
2019-12-15 15:12:56 -0600

    pierce reply added updated

--- week-3/discussion-3.1-Pierce-reply.js
+++ week-3/discussion-3.1-Pierce-reply.js
@@ -13,6 +13,7 @@ var i = 1; //declare variable value to be used in loop
 
 while (i < 11) { //condition for loop to continue
   console.log(i + " " + "This loop will count to 10"); // the thing to do
+  //corrected console log: added missing "+" and ending quotation mark
     i++; //increase var value by 1 each time
   }
 
