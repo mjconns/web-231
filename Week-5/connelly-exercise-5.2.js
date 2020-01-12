20d403eb4a2d54f130753edac9cda31db0b83b06
mjconns <mjconns@gmail.com>
2020-01-12 11:40:52 -0600

    exercise 5.2

---
+++ Week-5/connelly-exercise-5.2.js
@@ -0,0 +1,28 @@
+/*
+;============================================
+; Title:  week 5: exercise 5.2
+; Authors: Micah Connelly
+; Date:   12 January 2020
+; Modified By: Micah Connelly
+; Description:
+;===========================================
+*/
+
+/*
+; Expected output:
+; a list of my favorite foods
+*/
+
+// import
+const header = require('../week-2/header');
+console.log(header.display("Micah", "Connelly", "Exercise 5.2 - Header"));
+
+// variables
+var favFoods = ['Pasta', 'Pizza', 'Hamburgers', 'Burritos', 'Hotdogs'];
+
+// loop & output
+favFoods.forEach(function(favFoods) {
+  console.log(favFoods);
+});
+
+// end program
